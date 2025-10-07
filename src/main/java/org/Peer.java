83a943d100121172a700053ab9c2a22440512363
KhorchaniMarwa <marwa.khorchani@eniso.u-sousse.tn>
2025-10-07 22:19:15 +0100

    peer code

--- src/main/java/org/Peer.java
+++ src/main/java/org/Peer.java
@@ -1,4 +1,23 @@
 package org;
 
+import java.util.ArrayList;
+
 public class Peer {
+    private int peerID;
+    private int IP;
+    private int port;
+    private ArrayList<Peer> ownedFiles = new ArrayList();
+
+    public  Peer(int peerID,int port,int IP, ArrayList<Peer> ownedFiles){
+        this.peerID = peerID;
+        this.port = port;
+        this.IP = IP;
+        this.ownedFiles = ownedFiles;
+    }
+    
+    public static void main (String[] args){
+        PeerServer peerServer = new PeerServer(5000);
+        PeerClient pc = new PeerClient("127.0.0.1", 5000, "/home/marwa/keys.text");
+    }
+
 }
